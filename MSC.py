1d8721d7be0b4dba97d1a2540d5cecdd22e476de
Jalil Nourisa <jalil.nourisa@gmail.com>
2021-08-11 12:44:54 +0200

    IL10 regulatory data is added to observations

--- MSC.py
+++ MSC.py
@@ -5,63 +5,62 @@ import tellurium as te
 # %config Completer.use_jedi = False
 # showOriginalModelString = True
 #TODO: watch out that ctrl gets reset at each calibration run
-# free_params = {
-#     'TNFa_l': 5
-# }
+free_params = {
+    'IL10_L': [0,1], 
+    'IL10_H': [10,100],
+    'diff_time':[15,30], $
+}
 
 class Fuzzy_MSC:
     def __init__(self,params):
         self.antecedents = []
         self.consequents = []
         self.params = params
-        TNFa_0 = self.params['TNFa'][0]; TNFa_1 = self.params['TNFa'][1]; TNFa_end = self.params['TNFa'][2] 
+        # TNFa_0 = self.params['TNFa'][0]; TNFa_1 = self.params['TNFa'][1]; TNFa_end = self.params['TNFa'][2] 
         IL10_0 = self.params['IL10'][0]; IL10_end = self.params['IL10'][-1]; 
         #// define antecedents
-        TNFa = ctrl.Antecedent(np.arange(TNFa_0, TNFa_end, .05), 'TNFa')
+        # TNFa = ctrl.Antecedent(np.arange(TNFa_0, TNFa_end, .05), 'TNFa')
         IL10 = ctrl.Antecedent(np.arange(IL10_0, IL10_end, .05), 'IL10')
         #// define memberships
-        TNFa['L'] = fuzz.trimf(TNFa.universe, [TNFa_0, TNFa_0, TNFa_1])
-        TNFa['M'] = fuzz.trimf(TNFa.universe, [TNFa_0, TNFa_1, TNFa_end])
-        TNFa['H'] = fuzz.trimf(TNFa.universe, [TNFa_1, TNFa_end, TNFa_end])
-
-        IL10['L'] = fuzz.trimf(IL10.universe, [IL10_0, IL10_0, IL10_end])
-        IL10['H'] = fuzz.trimf(IL10.universe, [IL10_0, IL10_end, IL10_end])
+        # TNFa['L'] = fuzz.trimf(TNFa.universe, [TNFa_0, TNFa_0, TNFa_1])
+        # TNFa['M'] = fuzz.trimf(TNFa.universe, [TNFa_0, TNFa_1, TNFa_end])
+        # TNFa['H'] = fuzz.trimf(TNFa.universe, [TNFa_1, TNFa_end, TNFa_end])
+        IL10['N'] = fuzz.trimf(IL10.universe, [IL10_0, IL10_0,self.params['IL10'][1]])
+        IL10['L'] = fuzz.trimf(IL10.universe, [IL10_0, self.params['IL10'][1], self.params['IL10'][2]])
+        IL10['M'] = fuzz.trimf(IL10.universe, [self.params['IL10'][1], self.params['IL10'][2],self.params['IL10'][3]])
+        IL10['H'] = fuzz.trapmf(IL10.universe, [self.params['IL10'][2],self.params['IL10'][3], IL10_end, IL10_end])
         #// store
-        self.antecedents.append(TNFa)
+        # self.antecedents.append(TNFa)
         self.antecedents.append(IL10)
-#         IL10.view()
+        IL10.view()
 #         TNFa.view()
-        
+        exit(2)
         #// define consequents
         range_value = np.arange(0, 1, .01)
-        IL6 = ctrl.Consequent(range_value, 'IL6')
-        PGE2 = ctrl.Consequent(range_value, 'PGE2')
+        early_diff = ctrl.Consequent(range_value, 'early_diff')
+        late_diff = ctrl.Consequent(range_value, 'late_diff')
         
         #// define membership functions
         sigma = .05
         intervals = [0,.5, 1]
-        IL6['L']=fuzz.gaussmf(range_value, intervals[0], sigma)
-        IL6['M']=fuzz.gaussmf(range_value, intervals[1], sigma)
-        IL6['H']=fuzz.gaussmf(range_value, intervals[2], sigma)
-#         IL6.automf(3)
-        intervals = [0,.25,.5,.75, 1]
-        PGE2['VL']=fuzz.gaussmf(range_value, intervals[0], sigma)
-        PGE2['L']=fuzz.gaussmf(range_value, intervals[1], sigma)
-        PGE2['M']=fuzz.gaussmf(range_value, intervals[2], sigma)
-        PGE2['H']=fuzz.gaussmf(range_value, intervals[3], sigma)
-        PGE2['VH']=fuzz.gaussmf(range_value, intervals[4], sigma)
-#         PGE2.automf(5)
-        
+        early_diff['L']=fuzz.gaussmf(range_value, intervals[0], sigma)
+        early_diff['M']=fuzz.gaussmf(range_value, intervals[1], sigma)
+        early_diff['H']=fuzz.gaussmf(range_value, intervals[2], sigma)
+
+        late_diff['L']=fuzz.gaussmf(range_value, intervals[0], sigma)
+        late_diff['M']=fuzz.gaussmf(range_value, intervals[1], sigma)
+        late_diff['H']=fuzz.gaussmf(range_value, intervals[2], sigma)
+
         #// Store
-        self.consequents = [IL6,PGE2]
+        self.consequents = [early_diff,late_diff]
         #// rules
-        TNF_rules = [
+        early_diff_rules = [
         ctrl.Rule(TNFa['L'] , IL6['L']),
         ctrl.Rule(TNFa['M'] , IL6['M']),
         ctrl.Rule(TNFa['H'] , IL6['H'])
         ]
         
-        PGE2_rules = [
+        late_diff_rules = [
         ctrl.Rule( TNFa['L'] , PGE2['VL']),
         ctrl.Rule( TNFa['M'] & IL10['L'] , PGE2['L']),
         ctrl.Rule( TNFa['M'] & IL10['H'] , PGE2['M']),
@@ -69,7 +68,7 @@ class Fuzzy_MSC:
         ctrl.Rule( TNFa['H'] & IL10['H'] , PGE2['VH'])
         ]
         
-        rules = TNF_rules + PGE2_rules
+        rules = early_diff_rules + late_diff_rules
         self.controler = ctrl.ControlSystemSimulation(ctrl.ControlSystem(rules))
         
     def forward(self,inputs):
@@ -86,52 +85,51 @@ class Fuzzy_MSC:
 
         
 class MSC_model:
-    def __init__(self,free_params_keys,free_params_values,setup_info):
-        self.setup_info = setup_info
+    def __init__(self,free_params_keys,free_params_values,measurement_scheme,inputs):
+        self.inputs = inputs
+        self.measurement_scheme=measurement_scheme
         #// assmeble the key and values for the free params
         free_params = {}
         for key,value in  zip(free_params_keys,free_params_values):
             free_params[key] = value
 
         self.params = {
-            'TNFa': [0,free_params['TNF_a_l'],10],
-            'IL10': [0,1]
+            'IL10': [0,free_params['IL10_L'],(free_params['IL10_L']+free_params['IL10_H'])/2,free_params['IL10_H'],100]
         }
         self.params = {**self.params, **free_params}
         self.data = {'IL6':[0],'PGE2':[0]}
         self.fuzzy_model = Fuzzy_MSC(self.params)
-        self.ODE_model = te.loada("""
-            IL6 = 0;
-            IL6_prod = 1;
-            K_IL6_prod = 100;
-            K_IL6_sat = 100;
-            $IL6_prod -> IL6; K_IL6_prod - K_IL6_sat*IL6;
-            
-            PGE2 = 0;
-            PGE2_prod = 1;
-            K_PGE2_prod = 100;
-            K_PGE2_sat = 100;
-            $PGE2_prod -> PGE2; K_PGE2_prod - K_PGE2_sat*PGE2;
-            
-        """)
-
-
+        self.osteogenic_model = None
+    def osteogenesis(self,fuzzy_results):
+        ALP_checkpoint = self.measurement_scheme['ALP']*24 #time in hours
+        ARS_checkpoint = self.measurement_scheme['ARS']*24
+        def maturity_calculator(timepoint):
+            if timepoint < self.params['maturity_t']:
+                fuzzy_results['early_diff']*
+            else:
+        
+        ALP_maturity = ALP_checkpoint # maturity calculated at the given time point
     def run(self):
         #// get the coeff from fuzzy model
         fuzzy_results = self.fuzzy_model.forward(self.setup_info['inputs'])
-        f_IL6 = fuzzy_results['IL6']
-        f_PGE2 = fuzzy_results['PGE2']
+        self.osteogenesis(fuzzy_results)
+        # f_IL6 = fuzzy_results['IL6']
+        # f_PGE2 = fuzzy_results['PGE2']
         #// update the ODE model based on fuzzy coeff
-        self.ODE_model['K_IL6_prod'] = self.params['K_IL6_prod_0']*(1+self.params['K_IL6_prod_beta']*f_IL6)
-        self.ODE_model['K_PGE2_prod'] = self.params['K_PGE2_prod_0']*(1+self.params['K_PGE2_prod_beta']*f_PGE2)
+        # self.ODE_model['K_IL6_prod'] = self.params['K_IL6_prod_0']*(1+self.params['K_IL6_prod_beta']*f_IL6)
+        # self.ODE_model['K_PGE2_prod'] = self.params['K_PGE2_prod_0']*(1+self.params['K_PGE2_prod_beta']*f_PGE2)
 
-        self.ODE_model['K_IL6_sat'] = self.params['K_IL6_sat']
-        self.ODE_model['K_PGE2_sat'] = self.params['K_PGE2_sat']
+        # self.ODE_model['K_IL6_sat'] = self.params['K_IL6_sat']
+        # self.ODE_model['K_PGE2_sat'] = self.params['K_PGE2_sat']
         #// run the ODE model
-        selections = ['TIME','IL6','PGE2']
-        results = self.ODE_model.simulate(0,self.setup_info['duration'],self.setup_info['duration'],selections= selections)
+        # selections = ['TIME','IL6','PGE2']
+        # results = self.ODE_model.simulate(0,self.setup_info['duration'],self.setup_info['duration'],selections= selections)
         # self.ODE_model.plot()
         return {'IL6':results['IL6'][-1],'PGE2':results['PGE2'][-1]}
+
+free_params_values=[np.mean(item) for item in free_params.values()]
+
+obj = MSC_model(free_params_keys=free_params.keys(),free_params_values=free_params_values,setup_info=None)
 
 
 
--- observations.py
+++ observations.py
@@ -1,133 +1,373 @@
 observations = {
-	"IDs": ['MSC_0_0','MSC_0_.1','MSC_0_1','MSC_1_0','MSC_1_.1','MSC_1_1','MSC_10_0','MSC_10_.1','MSC_10_1'],
-	# "IDs": ['MSC_0_0','MSC_1_0','MSC_10_0'],
-
-	"MSC_0_0": {
-		"setup": {
-			'duration': 48, #h
-			'inputs':{
-				"TNFa": 0, #ng/ml
-				"IL10": 0
-			}
+	'studies':['Saldana_2019_part1','Valles_2020','Chen_2018'],
+	'Chen_2018':{# the effect of Il10 on ALP and ARS
+		'exposure_period':None, #when this is none, the cells are exposed to the stimuli the whole time
+		'culture_volume':None, #ml
+		'experiment_period':9, # days
+		'measurement_scheme':{
+			'ALP': 3, #day
+			'ARS': 9
 		},
-
-		"expectations": {			
-			"IL6": 0.5, #ng/ml
-			"PGE2": 0.03
-		}
-	},
-	"MSC_0_.1": {
-		"setup": {
-			'duration': 48, #h
+		'IDs': ['ctr','IL10_.01','IL10_.1','IL10_1','IL10_10','IL10_100'],
+		'ctr':{
 			'inputs':{
-				"TNFa": 0, #ng/ml
-				"IL10": .1
+						"IL10": 0
+					},
+			"expectations": {			
+				"ALP": {
+					'mean':2, 
+					'std': 0
+				},
+				"ARS": {
+					'mean':1, 
+					'std': .1
+				},
 			}
-		},
 
-		"expectations": {			
-			"IL6": 0.45, #ng/ml
-			"PGE2": 0.028
-		}
-	},
-	"MSC_0_1": {
-		"setup": {
-			'duration': 48, #h
+		},
+		'IL10_.01':{
 			'inputs':{
-				"TNFa": 0, #ng/ml
-				"IL10": 1
+						"IL10": 0.01
+					},
+			"expectations": {			
+				"ALP": {
+					'mean':2.5, 
+					'std': .5
+				},
+				"ARS": {
+					'mean':1.7, 
+					'std': .15
+				},
 			}
-		},
 
-		"expectations": {			
-			"IL6": 0.48, #ng/ml
-			"PGE2": 0.026
-		}
-	},
-	"MSC_1_0": {
-		"setup": {
-			'duration': 48, #h
+		},
+		'IL10_.1':{
 			'inputs':{
-				"TNFa": 1, #ng/ml
-				"IL10": 0
+						"IL10": 0.1
+					},
+			"expectations": {			
+				"ALP": {
+					'mean':5.6, 
+					'std': .2
+				},
+				"ARS": {
+					'mean':4, 
+					'std': .6
+				},
 			}
-		},
 
-		"expectations": {			
-			"IL6": 0.76, #ng/ml
-			"PGE2": 0.09
-		}
-	},
-	"MSC_1_.1": {
-		"setup": {
-			'duration': 48, #h
+		},
+		'IL10_1':{
 			'inputs':{
-				"TNFa": 1, #ng/ml
-				"IL10": .1
+						"IL10": 1
+					},
+			"expectations": {			
+				"ALP": {
+					'mean': 4.1, 
+					'std': .5
+				},
+				"ARS": {
+					'mean': 3, 
+					'std': .4
+				},
 			}
-		},
 
-		"expectations": {			
-			"IL6": 0.74, #ng/ml
-			"PGE2": 0.1
-		}
-	},
-	"MSC_1_1": {
-		"setup": {
-			'duration': 48, #h
+		},
+		'IL10_10':{
 			'inputs':{
-				"TNFa": 1, #ng/ml
-				"IL10": 1
+						"IL10": 1
+					},
+			"expectations": {			
+				"ALP": {
+					'mean': 1.2, 
+					'std': .3
+				},
+				"ARS": {
+					'mean': 0.5, 
+					'std': .1
+				},
 			}
-		},
 
-		"expectations": {			
-			"IL6": 0.64, #ng/ml
-			"PGE2": 0.12
-		}
-	},
-	"MSC_10_0": {
-		"setup": {
-			'duration': 48, #h
+		},
+		'IL10_100':{
 			'inputs':{
-				"TNFa": 10, #ng/ml
-				"IL10": 0
+						"IL10": 1
+					},
+			"expectations": {			
+				"ALP": {
+					'mean': 1.5, 
+					'std': .15
+				},
+				"ARS": {
+					'mean': 0.4, 
+					'std': .15
+				},
 			}
-		},
 
-		"expectations": {			
-			"IL6": 1.25, #ng/ml
-			"PGE2": 0.13
-		}
+		},
 	},
-	"MSC_10_.1": {
-		"setup": {
-			'duration': 48, #h
+	'Saldana_2019_part1':{# the effect of TNF-a and IL10 on ALP and ARS
+		'exposure_period':48, #hours
+		'culture_volume':2, #ml
+		'experiment_period':21, # days
+		'measurement_scheme':{
+			'ALP': 14, #day
+			'ARS': 21
+		},
+		
+		'IDs': ['ctr','TNFa_.1','TNFa_1','TNFa_10','IL10_.1','IL10_1','IL10_10'],
+		'ctr':{
 			'inputs':{
-				"TNFa": 10, #ng/ml
-				"IL10": .1
+						"TNFa": 0, #ng/ml
+						"IL10": 0
+					},
+			"expectations": {			
+				"ALP": {
+					'mean':200, 
+					'std': 40
+				},
+				"ARS": {
+					'mean':550, 
+					'std': 50
+				},
 			}
-		},
 
-		"expectations": {			
-			"IL6": 1.3, #ng/ml
-			"PGE2": 0.14
-		}
-	},
-	"MSC_10_1": {
-		"setup": {
-			'duration': 48, #h
+		},
+		'TNFa_.1':{
 			'inputs':{
-				"TNFa": 10, #ng/ml
-				"IL10": 1
+						"TNFa": .1, #ng/ml
+						"IL10": 0
+					},
+			"expectations": {			
+				"ALP": {
+					'mean':180, 
+					'std': 45
+				},
+				"ARS": {
+					'mean':570, 
+					'std': 55
+				},
 			}
-		},
 
-		"expectations": {			
-			"IL6": 1.15, #ng/ml
-			"PGE2": 0.175
+		},
+		'TNFa_1':{
+			'inputs':{
+						"TNFa": 1, #ng/ml
+						"IL10": 0
+					},
+			"expectations": {			
+				"ALP": {
+					'mean':300, 
+					'std': 45
+				},
+				"ARS": {
+					'mean':670, 
+					'std': 50
+				},
+			}
+
+		},
+		'TNFa_10':{
+			'inputs':{
+						"TNFa": 10, #ng/ml
+						"IL10": 0
+					},
+			"expectations": {			
+				"ALP": {
+					'mean':200, 
+					'std': 50
+				},
+				"ARS": {
+					'mean':520, 
+					'std': 50
+				},
+			}
+
+		},
+		'IL10_.1':{
+			'inputs':{
+						"TNFa": 0, #ng/ml
+						"IL10": 0.1
+					},
+			"expectations": {			
+				"ALP": {
+					'mean':290, 
+					'std': 25
+				},
+				"ARS": {
+					'mean':550, 
+					'std': 70
+				},
+			}
+
+		},
+		'IL10_1':{
+			'inputs':{
+						"TNFa": 0, #ng/ml
+						"IL10": 1
+					},
+			"expectations": {			
+				"ALP": {
+					'mean':350, 
+					'std': 70
+				},
+				"ARS": {
+					'mean':800, 
+					'std': 60
+				},
+			}
+
+		},
+		'IL10_10':{
+			'inputs':{
+						"TNFa": 0, #ng/ml
+						"IL10": 10
+					},
+			"expectations": {			
+				"ALP": {
+					'mean':420, 
+					'std': 80
+				},
+				"ARS": {
+					'mean':850, 
+					'std': 100
+				},
+			}
+
 		}
+
 	},
+	'Saldana_2019':{
+		"IDs": ['MSC_0_0','MSC_0_.1','MSC_0_1','MSC_1_0','MSC_1_.1','MSC_1_1','MSC_10_0','MSC_10_.1','MSC_10_1'],
+		"MSC_0_0": {
+				"setup": {
+					'duration': 48, #h
+					'inputs':{
+						"TNFa": 0, #ng/ml
+						"IL10": 0
+					}
+				},
+
+				"expectations": {			
+					"IL6": 0.5, #ng/ml
+					"PGE2": 0.03
+				}
+			},
+			"MSC_0_.1": {
+				"setup": {
+					'duration': 48, #h
+					'inputs':{
+						"TNFa": 0, #ng/ml
+						"IL10": .1
+					}
+				},
+
+				"expectations": {			
+					"IL6": 0.45, #ng/ml
+					"PGE2": 0.028
+				}
+			},
+			"MSC_0_1": {
+				"setup": {
+					'duration': 48, #h
+					'inputs':{
+						"TNFa": 0, #ng/ml
+						"IL10": 1
+					}
+				},
+
+				"expectations": {			
+					"IL6": 0.48, #ng/ml
+					"PGE2": 0.026
+				}
+			},
+			"MSC_1_0": {
+				"setup": {
+					'duration': 48, #h
+					'inputs':{
+						"TNFa": 1, #ng/ml
+						"IL10": 0
+					}
+				},
+
+				"expectations": {			
+					"IL6": 0.76, #ng/ml
+					"PGE2": 0.09
+				}
+			},
+			"MSC_1_.1": {
+				"setup": {
+					'duration': 48, #h
+					'inputs':{
+						"TNFa": 1, #ng/ml
+						"IL10": .1
+					}
+				},
+
+				"expectations": {			
+					"IL6": 0.74, #ng/ml
+					"PGE2": 0.1
+				}
+			},
+			"MSC_1_1": {
+				"setup": {
+					'duration': 48, #h
+					'inputs':{
+						"TNFa": 1, #ng/ml
+						"IL10": 1
+					}
+				},
+
+				"expectations": {			
+					"IL6": 0.64, #ng/ml
+					"PGE2": 0.12
+				}
+			},
+			"MSC_10_0": {
+				"setup": {
+					'duration': 48, #h
+					'inputs':{
+						"TNFa": 10, #ng/ml
+						"IL10": 0
+					}
+				},
+
+				"expectations": {			
+					"IL6": 1.25, #ng/ml
+					"PGE2": 0.13
+				}
+			},
+			"MSC_10_.1": {
+				"setup": {
+					'duration': 48, #h
+					'inputs':{
+						"TNFa": 10, #ng/ml
+						"IL10": .1
+					}
+				},
+
+				"expectations": {			
+					"IL6": 1.3, #ng/ml
+					"PGE2": 0.14
+				}
+			},
+			"MSC_10_1": {
+				"setup": {
+					'duration': 48, #h
+					'inputs':{
+						"TNFa": 10, #ng/ml
+						"IL10": 1
+					}
+				},
+
+				"expectations": {			
+					"IL6": 1.15, #ng/ml
+					"PGE2": 0.175
+				}
+			}
+	},
+
+	
 
 	"scale": 1,
 }
