69074db21706e7776d75c0e1a5bd470b5bba6ce6
janursa <jalil.nourisa@gmail.com>
2021-09-19 17:03:38 +0200

    Valles results need to be checked

--- batchscript.h
+++ batchscript.h
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --partition=all
-#SBATCH --time=00:30:00                           # Maximum time requested
+#SBATCH --time=00:60:00                           # Maximum time requested
 #SBATCH --nodes=1                                 # Number of nodes
 #SBATCH --chdir   ./      # directory must already exist!
-#SBATCH --job-name  SA
-#SBATCH --output    SA-%N-%j.out            # File to which STDOUT will be written
-#SBATCH --error     SA-%N-%j.err            # File to which STDERR will be written
+#SBATCH --job-name  calib
+#SBATCH --output    calib-%N-%j.out            # File to which STDOUT will be written
+#SBATCH --error     calib-%N-%j.err            # File to which STDERR will be written
 #SBATCH --mail-type ALL                           # Type of email notification- BEGIN,END,FAIL,ALL
 #SBATCH --mail-user jalil.nourisa@hzg.de            # Email to which notifications will be sent. It defaults to 
 unset LD_PRELOAD
--- observations.py
+++ observations.py
@@ -2,9 +2,10 @@ observations = {
 	# 'studies':['Qiao_2021_Mg'],
 	# 'studies': ['Qiao_2021_IL8_IL1b','Qiao_2021_IL8','Qiao_2021_IL1b'],
 	# 'studies':['Ber_2016'],
-	# 'studies':['Chen_2018'], #TODO: check the kill again
-	# 'studies':['Valles_2020_IL10'],#TODO: check the kill again
-	'studies':['Valles_2020_TNFa'], #TODO: check the kill again
+	# 'studies':['Chen_2018'],
+	'studies':['Valles_2020_IL10'],
+	# 'studies':['Valles_2020_TNFa'],
+	# 'studies':['Valles_2020_TNFa','Valles_2020_IL10'],
 	# 'studies':['Saldana_2019','Valles_2020_IL10','Valles_2020_TNFa','Chen_2018','Qiao_2021_IL8','Qiao_2021_IL1b'],
 
 
